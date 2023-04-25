2d6db3567964e8943c622b3f1be1f361169e6f7a
luisroblessorroche <luis.roblessorroche@alum.uca.es>
2023-04-25 19:04:06 +0200

    subiendo p2

--- Robles_Sorroche_Luis/P2/test-consola.d
+++ Robles_Sorroche_Luis/P2/test-consola.d
@@ -1,2 +1,2 @@
-test-consola.o: test-consola.cpp ../P1/fecha.hpp usuario.hpp articulo.hpp \
+test-consola.o test-consola.d: test-consola.cpp ../P1/fecha.hpp usuario.hpp articulo.hpp \
   ../P1/cadena.hpp tarjeta.hpp
